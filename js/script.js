5cf17f76708a6c972168fb8a8fee74759d3671bb
D-oxys <sanuzumaki12@gmail.com>
2022-11-25 09:21:44 +0700

    update darkmode

--- js/script.js
+++ js/script.js
@@ -11,10 +11,36 @@ ham.addEventListener('click', function () {
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixednav = header.offsetTop;
+    const toTop = document.querySelector("#to-top");
 
     if (window.pageYOffset > fixednav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
+}
+
+// dark mode toggle 
+const darkToggle = document.querySelector("#dark-toggle");
+const hmtl = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+    if (darkToggle.checked) {
+        hmtl.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        hmtl.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// toggel local storage
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
 }
