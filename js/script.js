2b73f99398f5058959b9394c32bf2f2c53eab716
D-oxys <sanuzumaki12@gmail.com>
2022-11-26 09:00:22 +0700

    loading update

--- js/script.js
+++ js/script.js
@@ -44,3 +44,35 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
     darkToggle.checked = false;
 }
+
+// SHETS API
+        const scriptURL =
+            'https://script.google.com/macros/s/AKfycbxj5xyRzrH8KhRl9QXVYKfqrwi6sUUkQGkBg4gW9YUczLy063aqDxEcvBWGMZjiTaiqKA/exec';
+        const form = document.forms['tailwind-contact-form'];
+        const btnKirim = document.querySelector('.btn-kirim');
+        const btnAllert = document.querySelector('.btn-allert');
+        const btnLoading = document.querySelector('.btn-loading');
+        var alert_del = document.querySelectorAll('.alert-del');
+        alert_del.forEach((x) =>
+            x.addEventListener('click', function () {
+                x.parentElement.classList.toggle('hidden');
+            })
+        );
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            btnKirim.classList.toggle('hidden');
+            btnLoading.classList.toggle('hidden');
+            fetch(scriptURL, {
+                    method: 'POST',
+                    body: new FormData(form)
+                })
+                .then(response => {
+                    btnKirim.classList.toggle('hidden');
+                    btnLoading.classList.toggle('hidden');
+                    btnAllert.classList.toggle('hidden');
+                    form.reset();
+                    console.log('Success!', response);
+                })
+                .catch(error => console.error('Error!', error.message));
+        })
